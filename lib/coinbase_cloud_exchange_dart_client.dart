08b7514fad286a9c3fff8357a2ec483fe3f8dc83
JaysonBH <jayson@hairstonsolutions.com>
2022-09-30 02:26:27 -0400

    Cloud Build Checks & Tests
    * Dart format checks
    * Run Dart Tests

---
+++ lib/coinbase_cloud_exchange_dart_client.dart
@@ -0,0 +1,7 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library coinbase_cloud_exchange_dart_client;
+
+export 'src/coinbase_cloud_exchange_dart_client_base.dart';
+export 'src/entities/products.dart';
