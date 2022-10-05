b19585d166b3c8bb8956e0b03cff1a52dbd99227
JaysonBH <jayson@hairstonsolutions.com>
2022-10-05 07:16:30 -0400

    Enable Authorized GET Requests
    * Credential class to contain dynamic Coinbase credentials
    * Pull coinbase credentials from environment variables within tests
    * Query Parameter conversion function to enable proper signatures requiring a specific string format
    * Add Query Parameter option to Unauthorized GET requests

--- lib/coinbase_cloud_exchange_dart_client.dart
+++ lib/coinbase_cloud_exchange_dart_client.dart
@@ -3,6 +3,7 @@
 /// More dartdocs go here.
 library coinbase_cloud_exchange_dart_client;
 
-export 'src/coinbase_cloud_exchange_dart_client_base.dart';
 export 'src/entities/products/products.dart';
 export 'src/entities/products/ticker.dart';
+export 'src/models/credential.dart';
+export 'src/models/signature.dart';
---
+++ lib/src/models/credential.dart
@@ -0,0 +1,7 @@
+class Credential {
+  final String accessKey;
+  final String secret;
+  final String passphrase;
+
+  Credential(this.accessKey, this.secret, this.passphrase);
+}
