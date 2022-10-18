b39e20f9d38ea9ecfebb7490a950fa232393e279
JaysonBH <jayson@hairstonsolutions.com>
2022-10-18 06:17:52 -0400

    Include all Object Models & Entities for export

--- lib/coinbase_cloud_exchange_dart_client.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library coinbase_cloud_exchange_dart_client;
-
-export 'src/entities/products/products.dart';
-export 'src/entities/products/ticker.dart';
-export 'src/models/credential.dart';
-export 'src/models/signature.dart';
---
+++ lib/models.dart
@@ -0,0 +1,9 @@
+library models;
+
+export 'src/models/credential.dart';
+export 'src/models/fee.dart';
+export 'src/models/fill.dart';
+export 'src/models/order.dart';
+export 'src/models/product.dart';
+export 'src/models/signature.dart';
+export 'src/models/ticker.dart';
