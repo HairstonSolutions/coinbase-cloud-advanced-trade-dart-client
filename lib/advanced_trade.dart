69da42274913a1b6c2790c250a53702a8c3a61eb
JaysonBH <jayson@hairstonsolutions.com>
2022-11-24 01:08:12 -0500

    Advanced Trade Starting section
    * AT Credential Model
    * AT Authenticated Signature Function
    * HTTPS Auth Get Framework

---
+++ lib/advanced_trade.dart
@@ -0,0 +1,10 @@
+library advanced_trade;
+
+export 'src/advanced_trade/models/credential.dart';
+export 'src/shared/models/account.dart';
+export 'src/shared/models/fee.dart';
+export 'src/shared/models/fill.dart';
+export 'src/shared/models/order.dart';
+export 'src/shared/models/product.dart';
+export 'src/shared/models/signature.dart';
+export 'src/shared/models/ticker.dart';
